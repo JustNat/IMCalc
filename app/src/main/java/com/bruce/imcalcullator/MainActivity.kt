a933384dcb8dca6ddcc6792e22cd21aaa17a9aac
JustNat <gabrielcp4@outlook.com>
2021-08-27 19:36:08 -0300

    aplicando o estado do imc (magreza, sobrepeso...)

--- app/src/main/java/com/bruce/imcalcullator/MainActivity.kt
+++ app/src/main/java/com/bruce/imcalcullator/MainActivity.kt
@@ -7,7 +7,6 @@ import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import kotlinx.android.synthetic.main.activity_main.*
 
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +17,7 @@ class MainActivity : AppCompatActivity() {
 
         cardResult.visibility = View.INVISIBLE
 
-        val calculate: View = findViewById(R.id.fab)
+        val calculate: View = fab
         calculate.setOnClickListener {
             calculateIMC(weight.text.toString(), height.text.toString())
         }
@@ -28,6 +27,11 @@ class MainActivity : AppCompatActivity() {
         val peso = peso.toFloatOrNull()
         val altura = altura.toFloatOrNull()
         when {
+
+            /* Mensagem toast caso todos os valores sejam nulos. Importante estar no ínicio para
+            não rodar as outras condições de nulo por último.
+             */
+
             (peso == null && altura == null) -> {
                 Toast.makeText(this, "Insira peso e altura", Toast.LENGTH_SHORT).show()
             }
@@ -35,13 +39,36 @@ class MainActivity : AppCompatActivity() {
                 val imc = peso / (altura * altura)
                 result.text = "IMC: %.2f".format(imc)
                 cardResult.visibility = View.VISIBLE
+
+                // Estado do IMC
+
+                when {
+                    (imc < 18.5) -> {
+                        imcState.text = "Magreza"
+                    }
+                    (imc in 18.5..24.9) -> {
+                        imcState.text = "Normal"
+                    }
+                    (imc in 25.0..29.9) -> {
+                        imcState.text = "Sobrepeso"
+                    }
+                    (imc in 30.0..39.9) -> {
+                        imcState.text = "Obesidade"
+                    }
+                    (imc >= 40) -> {
+                        imcState.text = "Obesidade grave"
+                    }
                 }
+            }
+
+            // Mensagem Toast caso algum dos dois valores sejam nulos (altura e peso).
+
             (peso == null) -> {
                 Toast.makeText(this, "Insira um peso", Toast.LENGTH_SHORT).show()
             }
             (altura == null) -> {
                 Toast.makeText(this, "Insira uma altura", Toast.LENGTH_SHORT).show()
             }
-            }
         }
     }
+}
